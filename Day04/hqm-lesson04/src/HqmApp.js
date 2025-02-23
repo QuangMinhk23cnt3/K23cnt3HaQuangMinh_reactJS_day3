efd021fcca6a17215de90209cfe2e81041060d85
Minh Quang <haquangminhk23cnt3@gmail.com>
2025-02-23 07:42:56 +0700

    hqm-day04

---
+++ Day04/hqm-lesson04/src/HqmApp.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import HqmFuncComp from './components/HqmFuncComp';
+import HqmFuncComp1 from './components/HqmFuncComp1';
+import HqmClassComp from './components/HqmClassComp';
+
+function HqmApp() {
+  const users = {
+    fullName:'Ha Quang Minh',
+    age:1000,
+    phone:"096969996"
+  }
+  return (
+    <div className="container bordermt-3">
+      <h1>Demo Component - Props - State</h1>
+      <hr/>
+      <div className='alert alert-danger'>
+          <HqmFuncComp name="Quang Minh" address="69 Say Gex" company="SayGex999" />
+          <hr/>
+          <HqmFuncComp name="Minh Quang" address="99 Say Gex" company="SayGex999" /> 
+      </div>
+      <div className='alert alert-danger'>
+          <HqmFuncComp1 renderInfo={users}/>
+      </div>
+      <HqmClassComp />
+      {/* chuyển dữ liệu từ HqmApp -> xuống HqmClassComp */}
+      <HqmClassComp renderName="K23cnt3" renderUsers={users} />
+    </div>
+  );
+}
+
+export default HqmApp;
---
+++ Day04/hqm-lesson04/src/components/HqmClassComp.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+class HqmClassComp extends Component {
+  constructor(props){
+    super(props);
+    //khoi tao state
+    this.state = {
+        fullName:"Minh Minh",
+        age:1000,
+        phone:"096969996"
+    }
+  }
+  //Ham xu ly su kien
+  changeInfo = (ev)=>{
+    // cap nhat state
+    this.setState({
+        fullName:"Ha Quang Minh"
+    });
+  }
+    render() {
+    return (
+      <div className='alert alert-success'>
+        <h2>Trình bày dữ liệu từ state</h2>
+                <p>info state (fullName): {this.state.fullName}</p>
+                <p>info state (age): {this.state.age}</p>
+                <p>info state (phone): {this.state.phone}</p>
+
+                <hr/>
+                <button className='btn btn-primary' onClick={this.changeInfo}>Change Info</button>
+                <hr/>
+                <h3>Lấy dữ liệu từ Props</h3>
+                <p>Name: {this.props.renderName}</p>
+               
+                <p>FullName:{ this.props.renderUsers ?this.props.renderUsers.fullName:''}</p>
+                <p>Age:{ this.props.renderUsers ?this.props.renderUsers.age:''}</p>
+                <p>Phone:{ this.props.renderUsers ?this.props.renderUsers.phone:''}</p>
+      </div>
+    )
+  }
+}
+
+export default HqmClassComp;
