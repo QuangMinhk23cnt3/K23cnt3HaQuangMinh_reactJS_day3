8c4c5e8d0b1f3962ca86ff9fa2d22e4cbfc27ed7
Minh Quang <haquangminhk23cnt3@gmail.com>
2025-03-19 14:53:10 +0700

    day13

---
+++ Day13/hqm_lesson13_api/src/HqmApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HqmReadApiLocal from './components/HqmReadApiLocal'
+
+export default function HqmApp() {
+  return (
+    <div className='container border my-3 p-3'>
+      <h1>ReactJS - API - Ha Quang Minh</h1>
+      <hr />
+      <HqmReadApiLocal />
+    </div>
+  )
+}
---
+++ Day14/hqm_lesson14/src/HqmApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HqmApp() {
+  return (
+    <div className='container border my-3 p-3'>
+      <h1>Toi co suc manh 500 trieu</h1>
+      <hr />
+    </div>
+  )
+}
