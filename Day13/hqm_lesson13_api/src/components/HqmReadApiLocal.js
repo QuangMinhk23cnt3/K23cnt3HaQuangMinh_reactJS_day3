4546b78e830393ef5d3acce9757d338867eb1a9e
Minh Quang <haquangminhk23cnt3@gmail.com>
2025-03-19 23:29:17 +0700

    day13

--- Day13/hqm_lesson13_api/src/components/HqmReadApiLocal.js
+++ Day13/hqm_lesson13_api/src/components/HqmReadApiLocal.js
@@ -1,48 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 export default function HqmReadApiLocal() {
-    // tao state 
-    const [hqmListUser, setHqmListUser] = useState([])
+    // khoi tao state
+    const [hqmListUser, hqmsetListUser] = useState([])
     // doc du lieu tu api local va set cho hqmlistuser
-    const apiUrl = "http://localhost:3001/users"
+    const apiUrl ="http://localhost:3001/users"
     // lay danh sach tu apiUrl
-    useEffect(() => {
+    useEffect(()=>{
         axios
             .get(apiUrl)
-            .then((hqmRespone) => {
-                setHqmListUser(hqmRespone.data)
+            .then((hqmRespone)=>{
+                hqmsetListUser(hqmRespone.data)
             })
-            .catch((error) => {
+            .catch((error)=>{
                 console.log("Loi");
             })
     },[])
-    return (
-        <div>
-            <h2>Doc du lieu tu API Local</h2>
-            <table className='table table-bordered'>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>FullName</th>
-                        <th>Age</th>
-                        <th>Class</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        hqmListUser.map((hqmItem, index)=>{
-                            return (
-                                <tr>
-                                    <td>{hqmItem.id}</td>
-                                    <td>{hqmItem.fullName}</td>
-                                    <td>{hqmItem.age}</td>
-                                    <td>{hqmItem.class}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+
+  return (
+    <div>
+        <h2>Doc du lieu tu API Local</h2>
+        <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>FullName</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    hqmListUser.map((hqmItem, index)=>{
+                        return (
+                            <tr>
+                                <td>{hqmItem.fullName}</td>
+                                <td>{hqmItem.age}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    </div>
+  )
 }
